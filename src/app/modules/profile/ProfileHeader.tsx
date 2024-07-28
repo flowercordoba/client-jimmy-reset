db4940f063946911d937d6802a30bebd9798f2c0
Manuel Carvajal <manuel.c1992@gmail.com>
2024-07-28 15:03:01 -0500

    implement CoverImage component

--- src/app/modules/profile/ProfileHeader.tsx
+++ src/app/modules/profile/ProfileHeader.tsx
@@ -1,44 +1,18 @@
 import { FC } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Content } from "../../../features/layout/components/content";
-// import { useAuth } from "../auth";
 import ProfileImage from "./pages/accounts/ProfileImage";
+import CoverImage from "./pages/accounts/CoverImage";
 
 const ProfileHeader: FC = () => {
   const location = useLocation();
-  // const { currentUser } = useAuth();
 
   return (
     <>
       <Content>
         <div className="card mb-5 mb-xl-10">
           <div className="card-body pt-9 pb-0">
-            <div className="rounded w-100 mb-7" style={{ position: 'relative', height: '300px', overflow: 'hidden' }}>
-                <img 
-                  src="https://images.unsplash.com/photo-1721332149274-586f2604884d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                  alt="Portada"
-                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                />
-                <div
-                  className="position-absolute"
-                  style={{
-                    bottom: "10px",
-                    right: "10px",
-                    backgroundColor: "white",
-                    borderRadius: "50%",
-                    padding: "10px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    boxShadow: "0px 0px 5px rgba(0,0,0,0.2)",
-                    cursor: "pointer",
-                    zIndex: 1
-                  }}
-                  // onClick={handleShow}
-                >
-                  <i className="fas fa-camera" style={{ fontSize: "16px", color: "black" }}></i>
-                </div>
-              </div>
+            <CoverImage />
             <div className="d-flex flex-wrap flex-sm-nowrap mb-3">
               <div className="me-7 mb-4" style={{ marginTop: '-80px' }}>
                 <ProfileImage />
